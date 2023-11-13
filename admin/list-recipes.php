07e0be857512d94cf9860139aa6cf66b25382eb7
ibarrakeyler <ibarrakeyler@gmail.com>
2023-11-12 18:55:53 -0600

    se crea la lista que permite editar y eliminar

---
+++ admin/list-recipes.php
@@ -0,0 +1,34 @@
+<?php 
+    require_once '../database.php';
+    // Reference: https://medoo.in/api/select
+    $items = $database->select("tb_recipes","*");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registered Destinations</title>
+    <link rel="stylesheet" href="../css/mainRecipes.css">
+</head>
+<body>
+    <?php 
+        include "../parts/header-admin.php";
+    ?>
+    <h2 class="recipe-title">Registered Recipes</h2>
+    <table class="recipe-text">
+        <?php
+            foreach($items as $item){
+                echo "<tr>";
+                echo "<td>".$item["recipe_name"]."</td>";
+                echo "<td><a href='edit-destination.php?id=".$item["id_recipe"]."'>Edit</a> <a href='delete-destination.php?id=".$item["id_recipe"]."'>Delete</a></td>";
+                echo "</tr>";
+            }
+        ?>
+    </table>
+    
+    <?php 
+        include "../parts/footer.php";
+    ?>
+</body>
+</html>
